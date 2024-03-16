76fdc38db791a508832eb17081dcd7d6c27e6bcc
Vishal Jain <vj279@cam.ac.uk>
2024-03-16 21:09:25 +0000

    Initial commit - Add Refactored train.py
    Refactored original code across train.py, models.py and utils.py.
    Goal is to design a framework to quickly model the decoder, beta schedule
    and degradation process used for the diffusion model. Chose to design the
    code base so that input to the train script is just a config for reproducability.
    
    Train script outputs a single runs dir which contains all the relevant output of a job.
    
    Use weights and biases for logging.
    
    Have yet to implement plotting logic to visualise unconditional and conditional generation.

---
+++ src/utils.py
@@ -0,0 +1,59 @@
+import torch
+import random
+import numpy as np
+import os
+from models import CNN, DDPM
+
+
+def seed_everything(seed):
+    # Set seed for all packages with built-in pseudo-raiAndom generators.
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    # If using CUDA, set also the below for determinism.
+    if torch.cuda.is_available():
+        # Sets the seed for generating random numbers for the current GPU.
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)  # if using multi-GPU.
+
+        # Ensures that the CUDA convolution uses deterministic algorithms.
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+
+def safe_create_dir(dir_path):
+    if not os.path.isdir(dir_path):
+        print(f"[INFO] {dir_path} does not exit, making dir(s)")
+        os.makedirs(dir_path)
+    else:
+        if os.listdir(dir_path):
+            print(f"[ERROR] Files exist in {dir_path}... exiting")
+            exit(1)
+
+
+def fetch_model(model_key):
+    # TODO: move to a general diffusion model class init
+    model_dict = {
+        "cnn": CNN,
+        "ddpm": DDPM,
+    }
+
+    return model_dict[model_key]
+
+
+def fetch_beta_schedule(schedule_key):
+    fetch_beta_schedule_dict = {"linear": linear_beta_schedule}
+    return fetch_beta_schedule_dict[schedule_key]
+
+
+def linear_beta_schedule(T, beta1, beta2):
+    # NOTE: T must be the first argument for it to be initialised properly
+    # in the train.py script.
+    """Returns pre-computed schedules for DDPM sampling with a linear noise schedule."""
+    assert beta1 < beta2 < 1.0, "beta1 and beta2 must be in (0, 1)"
+
+    beta_t = (beta2 - beta1) * torch.arange(0, T, dtype=torch.float32) \
+        / (T - 1) + beta1
+
+    return beta_t
