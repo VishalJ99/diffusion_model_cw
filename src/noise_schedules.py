1f657e99ccc7fb72ea1b06692e5efed7e4c46d9b
Vishal Jain <vj279@cam.ac.uk>
2024-03-26 21:22:55 +0000

    Add Docstrings to utils.py and noise_schedules.py

--- src/noise_schedules.py
+++ src/noise_schedules.py
@@ -1,26 +1,66 @@
 import numpy as np
 
-# NOTE: T must be the first argument for it to be initialised properly
-# in the train.py script.
-# Implementations for beta_t and alpha_t are all of length T+1 to make the indexing
-# more intuitive. This way, beta_t[t] is the value of beta at time t.
-# All functions need to return np arrays of dtype np.float32, by default
-# np creates arrays of dtype np.float64 which can not be converted to tensors
-# on the mps device.
+"""
+Can define any custom noise schedule here and add it to the dict in
+fetch_noise_schedule function in utils.py to use it in the training script.
+Just pass it as a string in the config file and it will be fetched.
+
+To define a custom noise schedule, create a function that takes T as the first
+first argument for it to be initialised properly, any kwargs can be passed
+after in the config.
+
+All Implementations for beta_t and alpha_t are all of length T+1
+to make the indexing more intuitive. This way, beta_t[t] is the value of beta at time t.
+
+All functions need to return np arrays of dtype np.float32, by default
+np creates arrays of dtype np.float64 which can not be converted to tensors
+on the mps device.
+"""
 
 
-def stable_cumprod(x):
-    # Cumprod in log-space (better precision).
+def stable_cumprod(x: np.ndarray) -> np.ndarray:
+    """
+    Performs a cumulative product of x, but in logspace with numerical stability.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        The input array to perform the cumulative product on.
+
+    Returns
+    -------
+    np.ndarray
+        The cumulative product of x.
+    """
     return np.exp(np.cumsum(np.log(x)))
 
 
-def linear_noise_schedule(T, beta_1, beta_2):
+def linear_noise_schedule(
+    T: int, beta_1: int, beta_2: int
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Linear noise schedule for beta_t and alpha_t.
+
+    Parameters
+    ----------
+    T : int
+        The total number of timesteps.
+    beta_1 : int
+        The initial value of beta.
+    beta_2 : int
+        The final value of beta.
+
+    Returns
+    -------
+    tuple[np.ndarray, np.ndarray]
+        The beta_t and alpha_t values for the noise schedule.
+    """
     assert beta_1 < beta_2 < 1.0, "beta1 and beta2 must be in (0, 1)"
 
     beta_t = (beta_2 - beta_1) * np.arange(0, T, dtype=np.float32)
     beta_t /= T - 1
     beta_t += beta_1
-    
+
     alpha_t = stable_cumprod(1 - beta_t)
 
     beta_t = np.insert(beta_t, 0, 0)
@@ -29,11 +69,31 @@ def linear_noise_schedule(T, beta_1, beta_2):
     return beta_t, alpha_t
 
 
-def cosine_noise_schedule(T, beta_max=0.1, s=0.008):
-    # https://arxiv.org/abs/2102.09672
-    # TODO calc optimal value for s for MNIST data.
-    # s is a small constant to prevent beta_t from becoming too large.
-    # for small t. Should be set so sqrt(beta_1) = 1 / pixel bin width
+def cosine_noise_schedule(
+    T: int, beta_max: int = 0.9, s: int = 0.008
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Generates a cosine noise schedule for the given parameters. Used in
+    Paper - https://arxiv.org/abs/2102.09672 to give better results for training
+    diffusion models.
+    TODO: Calculate optimal value for s for MNIST data.
+
+    Parameters
+    ----------
+    T : int
+        The total number of time steps.
+    beta_max : int, optional
+        The maximum value for beta_t. Defaults to 0.9.
+    s : int, optional
+        A small constant to prevent beta_t from becoming too large for small t.
+        Should be set so sqrt(beta_1) = 1 / pixel bin width. Defaults to 0.008.
+
+    Returns
+    -------
+    tuple[np.ndarray, np.ndarray]
+        The beta_t and alpha_t values for the noise schedule.
+    """
+
     def f(t, s):
         freq = ((t / T) + s) / (4 * (1 + s))
         return np.cos(2 * np.pi * freq) ** 2
@@ -44,13 +104,29 @@ def cosine_noise_schedule(T, beta_max=0.1, s=0.008):
     beta_t = np.insert(beta_t, 0, 0).astype(np.float32)
     alpha_t = np.asarray(alpha_t).astype(np.float32)
 
-    # Clip beta_t vals greater than beta_max since beta explodes at the end.
+    # Clip beta_t values greater than beta_max since beta explodes at the end.
     beta_t = np.clip(beta_t, 0.0, beta_max)
 
     return beta_t, alpha_t
 
 
-def const_noise_schedule(T, beta):
+def const_noise_schedule(T: int, beta: int) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Constant noise schedule for beta_t and alpha_t.
+
+    Parameters
+    ----------
+    T : int
+        The total number of timesteps.
+
+    beta : int
+        The constant value of beta.
+
+    Returns
+    -------
+    tuple[np.ndarray, np.ndarray]
+        The beta_t and alpha_t values for the noise schedule.
+    """
     beta_t = np.full(T + 1, beta, dtype=np.float32)
     alpha_t = stable_cumprod(1 - beta_t)
     return beta_t, alpha_t
--- src/utils.py
+++ src/utils.py
@@ -12,10 +12,22 @@ from noise_schedules import (
     cosine_noise_schedule,
 )
 from piq import ssim, psnr
+from typing import Callable, List
 
 
-def seed_everything(seed):
-    # Set seed for all packages with built-in pseudo-raiAndom generators.
+def seed_everything(seed: int) -> None:
+    """
+    Seed everything for reproducibility.
+
+    Parameters
+    ----------
+    seed : int
+        The seed to use for reproducibility.
+
+    Returns
+    -------
+    None
+    """
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
@@ -31,7 +43,24 @@ def seed_everything(seed):
         torch.backends.cudnn.benchmark = False
 
 
-def safe_create_dir(dir_path):
+def safe_create_dir(dir_path: str) -> None:
+    """
+    Create a directory if it does not exist, if it exists and is not empty, print error
+    and exit.
+
+    TODO:
+    Consider raising an exception instead of exiting.
+    Consider adding a force flag to delete the directory if it exists and is not empty.
+
+    Parameters
+    ----------
+    dir_path : str
+        The path to the directory to create.
+
+    Returns
+    -------
+    None
+    """
     if not os.path.isdir(dir_path):
         print(f"[INFO] {dir_path} does not exit, making dir(s)")
         os.makedirs(dir_path)
@@ -41,21 +70,53 @@ def safe_create_dir(dir_path):
             exit(1)
 
 
-def fetch_model(model_key):
-    # TODO: move to a general diffusion model class init.
+def fetch_model(model_key: str) -> torch.nn.Module:
+    """
+    Define a dictionary of models and return the model class based on the key.
+    Used to fetch the model class based on the key in the config file by the
+    train.py, test.py and calc_fid.py scripts.
+
+    TODO: move to a general diffusion model class init.
+
+    Parameters
+    ----------
+    model_key : str
+        The key to use to fetch the model class.
+
+    Returns
+    -------
+    torch.nn.Module
+        The model class to use for training.
+    """
     model_dict = {
         "cnn": CNN,
         "ddpm": DDPM,
         "unet": Unet,
         "gaussian_blur": GaussianBlurDM,
-        "fashion_mnist": FashionMNISTDM
+        "fashion_mnist": FashionMNISTDM,
     }
 
     return model_dict[model_key]
 
 
-def fetch_noise_schedule(schedule_key):
-    # TODO: move to a more intuitive place.
+def fetch_noise_schedule(schedule_key: str) -> Callable:
+    """
+    Define a dictionary of noise schedules and return the noise schedule function based
+    on the key. Allows for custom noise schedules to be passed in the config file and
+    used by train.py, test.py and calc_fid.py scripts.
+
+    TODO: move to a more intuitive place.
+
+    Parameters
+    ----------
+    schedule_key : str
+        The key to use to fetch the noise schedule function.
+
+    Returns
+    -------
+    function
+        The noise schedule function to use for training.
+    """
     noise_schedule_dict = {
         "const": const_noise_schedule,
         "linear": linear_noise_schedule,
@@ -63,13 +124,36 @@ def fetch_noise_schedule(schedule_key):
     }
     return noise_schedule_dict[schedule_key]
 
-"""
-TODO: Add winsorisation before any normalisation, make a seperate function for this
-seems to be a common operation, also vectorise it instead of looping over the batch.
-"""
 
+def make_cond_samples_plot(
+    z_t: torch.tensor, visualise_ts: List[int], nrows: int, fs: int = 12
+) -> plt:
+    """
+    Makes a nicely formatted conditional sampling plot for the diffusion model.
+    Winsorises and normalises the images in the batch before plotting as plt.imshow
+    clips the values to [0, 1] which can make the images look washed out.
 
-def make_cond_samples_plot(z_t, visualise_ts, nrows, fs=12):
+    Parameters
+    ----------
+    z_t : torch.tensor
+        The tensor of images to plot. First dimension should be contiguous in the time
+        step parameter t. Shape should be [B, C, H, W].
+
+    visualise_ts : List[int]
+        The time steps to visualise.
+
+    nrows : int
+        The number of rows to use in the grid.
+
+    fs : int
+        The font size to use for the labels.
+
+    Returns
+    -------
+    plt
+        The matplotlib plot object.
+    """
+
     # Normalise every image in the batch.
     for idx, img in enumerate(z_t):
         img = winsorize_tensor(img, 1, 99)
@@ -107,7 +191,7 @@ def make_cond_samples_plot(z_t, visualise_ts, nrows, fs=12):
             transform=ax.transData,
         )
 
-    # Draw curly braces and add labels for "Degradation" and "Restoration"
+    # Add labels for "Degradation" and "Restoration"
     # Note: This requires manual adjustment for positioning
     degradation_label_pos_y = (len(visualise_ts) / 2) * (image_height + pad) + vpad
     restoration_label_pos_y = ((len(visualise_ts) * 2) / 2) * (image_height + pad) * 2
@@ -135,7 +219,25 @@ def make_cond_samples_plot(z_t, visualise_ts, nrows, fs=12):
     return plt
 
 
-def make_uncond_samples_plot(xh, nrows):
+def make_uncond_samples_plot(xh: torch.tensor, nrows: int) -> plt:
+    """
+    Make an unconditional sampling plot for the diffusion model.
+    Winsorises and normalises the images in the batch before plotting as plt.imshow
+    clips the values to [0, 1] which can make the images look washed out.
+
+    Parameters
+    ----------
+    xh : torch.tensor
+        The tensor of images to plot. Shape should be [B, C, H, W].
+
+    nrows : int
+        The number of rows to use in the grid.
+
+    Returns
+    -------
+    plt
+        The matplotlib plot object.
+    """
     # Normalise every image in the batch.
     for idx, img in enumerate(xh):
         img = winsorize_tensor(img, 1, 99)
@@ -151,17 +253,35 @@ def make_uncond_samples_plot(xh, nrows):
     return plt
 
 
-def calc_image_quality_metrics(x, x_hat):
+def calc_image_quality_metrics(
+    x: torch.tensor, x_hat: torch.tensor
+) -> tuple[float, float, float]:
+    """
+    Calculate the image quality metrics for a batch of images and their reconstructions.
+
+    Parameters
+    ----------
+    x : torch.tensor
+        The original images. Shape should be [B, C, H, W].
+
+    x_hat : torch.tensor
+        The reconstructed images. Shape should be [B, C, H, W].
+
+    Returns
+    -------
+    tuple[float, float, float]
+        The average RMSE, SSIM and PSNR scores for the batch.
+    """
     # TODO: vectorise this. Saving mean so dont need individual scores.
     # Can vectorise the normalisation step, metric functions are already vectorised.
     for idx in range(len(x)):
         src_img = x[idx]
         rec_img = x_hat[idx]
-        
+
         # Normalise the images.
         src_img = (src_img - src_img.min()) / (src_img.max() - src_img.min())
         rec_img = (rec_img - rec_img.min()) / (rec_img.max() - rec_img.min())
-        
+
         x[idx] = src_img
         x_hat[idx] = rec_img
 
@@ -173,7 +293,8 @@ def calc_image_quality_metrics(x, x_hat):
     metrics = (rmse_score, ssim_score, psnr_score)
     return metrics
 
-# Test this!
+
+# Test this! Chat GPT code!
 # def normalise_batch(batch):
 #     # Assumes batch is of shape [N, C, H, W]
 #     min_vals = torch.min(batch.view(batch.size(0), -1), dim=1)[0].view(-1, 1, 1, 1)
@@ -181,15 +302,39 @@ def calc_image_quality_metrics(x, x_hat):
 #     return (batch - min_vals) / (max_vals - min_vals)
 
 
-def winsorize_tensor(tensor, lower_percentile, upper_percentile):
+def winsorize_tensor(
+    tensor: torch.tensor, lower_percentile: int, upper_percentile: int
+) -> torch.tensor:
+    """
+    ChatGPT Generated Code.
+    Winsorize a tensor by clipping the values to the lower and upper percentiles.
+    Percentiles are expressed as values between 0 and 100.
+
+    Parameters
+    ----------
+    tensor : torch.tensor
+        The tensor to winsorize.
+
+    lower_percentile : int
+        The lower percentile to clip the values to.
+
+    upper_percentile : int
+        The upper percentile to clip the values to.
+
+    Returns
+    -------
+    torch.tensor
+        The winsorized tensor.
+    """
+
     # Calculate the percentile values
     lower_bound = torch.quantile(tensor, lower_percentile / 100.0)
     upper_bound = torch.quantile(tensor, upper_percentile / 100.0)
 
     # Winsorize the tensor
     winsorized_tensor = torch.where(tensor < lower_bound, lower_bound, tensor)
-    winsorized_tensor = torch.where(tensor > upper_bound, upper_bound,
-                                    winsorized_tensor)
-    
-    return winsorized_tensor
+    winsorized_tensor = torch.where(
+        tensor > upper_bound, upper_bound, winsorized_tensor
+    )
 
+    return winsorized_tensor
